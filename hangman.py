56b545c0cfa09e4352c81fba439d1be1099f5388
Riellytruffa <54604958+Riellytruffa@users.noreply.github.com>
2022-12-12 14:18:50 -0500

    commit2

--- hangman.py
+++ hangman.py
@@ -2,32 +2,55 @@ import random
 from words import words
 import string 
 
+
+
 def get_valid_word(words): 
-    word = random.choice(words) 
+    word = random.choice(words)  # chooses random word from words.py file 
     while '-' in word or ' ' in word: 
         word = random.choice(words)
-
-        return word 
+        
+        
+        return word.upper()
 
 def hangman(): 
     word = get_valid_word(words) 
-    word_letters = set(word) 
+    word_letters = set(word)  # Amount of letters
     alphabet = set(string.ascii_uppercase) 
-    used_letters = set()
+    used_letters = set()  # Used Letters
 
+    lives = 6
+
+    
+    while len(word_letters) > 0 and lives > 0: 
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
+    word_list = [letter if letter in used_letters else '-' for letter in word]
+    print('Current word: ', ' '.join(word_list))
+    
+    
     user_letter = input('Guess a letter: ').upper()
     if user_letter in alphabet - used_letters:
         used_letters.add(user_letter)
         if user_letter in word_letters:
             word_letters.remove(used_letters)
+                
+        else:
+                lives = lives - 1 
+                print('Letter is not in the word!')
+    
     
     elif user_letter in used_letters:
-        print('You have already used that character. Please try again. ')
-
+        print('You have already used that character. Please try again.') # User uses same character
+    
+    
     else:
-        print('Invalid character. Please try again')
+        print('Invalid character. Please try again') 
+    
+    if lives == 0:
+     print('You died, sorry. The word was', word)
+    print('You guessed the word', word, '!!')
 
-user_input = input('Type something ')
+
+user_input = input('Type something: ')
 print(user_input)
 
 
